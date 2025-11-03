5e7b591131d43bd06dcfc842b7082c81a17707d7
woou <hxx_ara@naver.com>
2025-11-03 09:41:39 +0900

    게시글 상세보기 요청 작성. 게시글 검색 처리 작성 중
    오류수정완료 판

--- src/main/java/com/codeit/blog/entity/Comment.java
+++ src/main/java/com/codeit/blog/entity/Comment.java
@@ -1,7 +1,16 @@
 package com.codeit.blog.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Comment {
 
     private Long id;
